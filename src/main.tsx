6451b1b90098eba07b2caf629d1dd27feaff5844
Jacky Daniel's <kml93@msn.com>
2026-04-07 12:44:39 +0200

    chore: initial project setup with React, Tailwind v4, ShadCN/UI and Monkey plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(
+  (() => {
+    const app = document.createElement('div');
+    app.id = 'skyscanner-car_rental';
+    document.body.append(app);
+    return app;
+  })(),
+).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
+import monkey from 'vite-plugin-monkey';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    monkey({
+      entry: 'src/main.tsx',
+      userscript: {
+        icon: 'https://vitejs.dev/logo.svg',
+        namespace: 'npm/vite-plugin-monkey',
+        match: ['https://www.google.com/'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
